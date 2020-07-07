a96fdfdcfc266a47a38c547474e1838334a8e818
SijyKijy <max.monovcov@gmail.com>
2020-07-07 23:54:52 +0300

    Logic

---
+++ Data/DotaService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using KirillPlusDota.Properties;
+
+namespace KirillPlusDota.Data
+{
+    public class DotaService
+    {
+        public async Task<string> GetGame()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var json = await client.GetAsync($"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={Resources.Token}&steamids=76561198034591874").Result.Content.ReadAsStringAsync();
+
+                if (json.Contains("gameextrainfo")) return JObject.Parse(json)["response"]["players"][0]["gameextrainfo"].ToString();
+
+                return string.Empty;
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using KirillPlusDota.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using KirillPlusDota.Data;
 
 namespace KirillPlusDota
 {
@@ -28,7 +22,7 @@ namespace KirillPlusDota
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<DotaService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
